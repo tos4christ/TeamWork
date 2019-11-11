869369dc3af6a3a5510d9d35633c18fa8382401d
Tos4christ <glmmission@hotmail.com>
2019-11-11 14:54:53 +0100

    chore(set up JWT and Postgreql) test passed on API end points

--- app/app.js
+++ app/app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+    console.log('the error object', err)
     res.status(err.status).send({
       'Server Error': err.message
     });
