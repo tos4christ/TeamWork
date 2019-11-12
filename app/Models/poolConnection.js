5cf9a83ffe97a511f02215592b8772bceaa20027
Tos4christ <glmmission@hotmail.com>
2019-11-12 14:25:40 +0100

    modified pool connection for the database

--- app/Models/poolConnection.js
+++ app/Models/poolConnection.js
@@ -4,13 +4,7 @@ import { Pool } from 'pg';
 // const query2 = require('./2ndpoolQuery');
 import query2 from './2ndpoolQuery';
 
-const pool = new Pool({
-  host: 'ec2-174-129-253-144.compute-1.amazonaws.com',
-  user: 'bncamlwesysfrq',
-  password: '70aa1093d8b60709294c6286d8cddf3431fcf90dafe9eb2b14c0b590fb82e5e9',
-  database: 'd1pe9mpgmslihs',
-  port: '5432'
-});
+const pool = new Pool('postgres://bncamlwesysfrq:70aa1093d8b60709294c6286d8cddf3431fcf90dafe9eb2b14c0b590fb82e5e9@ec2-174-129-253-144.compute-1.amazonaws.com:5432/d1pe9mpgmslihs');
 
 pool.on('connect', () => console.log('connected to the database'));
 
--- build/Models/poolConnection.js
+++ build/Models/poolConnection.js
@@ -13,13 +13,7 @@ var _ndpoolQuery2 = _interopRequireDefault(_ndpoolQuery);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // const { Pool } = require('pg');
-var pool = new _pg.Pool({
-  host: 'ec2-174-129-253-144.compute-1.amazonaws.com',
-  user: 'bncamlwesysfrq',
-  password: '70aa1093d8b60709294c6286d8cddf3431fcf90dafe9eb2b14c0b590fb82e5e9',
-  database: 'd1pe9mpgmslihs',
-  port: '5432'
-});
+var pool = new _pg.Pool('postgres://bncamlwesysfrq:70aa1093d8b60709294c6286d8cddf3431fcf90dafe9eb2b14c0b590fb82e5e9@ec2-174-129-253-144.compute-1.amazonaws.com:5432/d1pe9mpgmslihs');
 
 // const query2 = require('./2ndpoolQuery');
 
