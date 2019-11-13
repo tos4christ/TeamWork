90cf5de3b2fb4d06f1d3c0c1d41c58cf17b6819d
Tos4christ <glmmission@hotmail.com>
2019-11-13 12:51:45 +0100

    updated test spec files for test whose values need to change

--- app/spec/spec.js
+++ app/spec/spec.js
@@ -26,93 +26,10 @@ describe('Server setup', () => {
   });
 });
 
-describe('JWT route protection', () => {
-  let server;
-  beforeAll(() => {
-    server = servers;
-  });
-  describe('unauthenticated user should not access protected routes', () => {
-    const data = {};
-    beforeAll((done) => {
-      Request.post({
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        url: 'http://localhost:3000/api/v1/articles',
-        form: { title: 'test article', article: 'this is a test article', appr_status: true },
-      }, (err, res, body) => {
-        if (err) throw err;
-        data.status = res.statusCode;
-        data.body = body;
-        done();
-      });
-    });
-    it('should return error 401', () => {
-      expect(data.status).toBe(401);
-    });
-  });
-  describe('authenticated user should access protected routes', () => {
-    const data = {};
-    beforeAll((done) => {
-      Request.post({
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
-        },
-        url: 'http://localhost:3000/api/v1/articles',
-        form: { title: 'test articles', article: 'this is a test article', appr_status: true },
-      }, (err, res, body) => {
-        if (err) throw err;
-        data.status = res.statusCode;
-        data.body = JSON.parse(body);
-        done();
-      });
-    });
-    it('Should return status 201', () => {
-      expect(data.status).toBe(201);
-    });
-    it('Should return a user', () => {
-      expect(data.body.data.title).toBe('test articles');
-    });
-  });
-});
-
 describe('API endpoint tests', () => {
   let server;
   beforeAll(() => {
     server = servers;
-  });
-  describe('POST /auth/create-user', () => {
-    const data = {};
-    beforeAll((done) => {
-      Request.post({
-        headers: {
-          'content-type': 'x-www-form-urlencoded',
-          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzM1ODE5MjAsImV4cCI6MTU3NTAyMTkyMH0.js2EySKM5sykjUELWrA3jD2acVSwU_1lDooVIGKbp24',
-        },
-        url: 'http://localhost:3000/api/v1/auth/create-user',
-        form: {
-          firstname: 'bolanle1',
-          lastname: 'adeka1',
-          email: 'bolanle@gmail.com1',
-          employee_password: 'bolanle1',
-          gender: 'female',
-          jobrole: 'I.T',
-          employee_no: 29303941,
-          department: 'Admin',
-        },
-      }, (err, res, body) => {
-        if (err) throw err;
-        data.status = res.statusCode;
-        data.body = JSON.parse(body);
-        console.log('the body', data);
-        done();
-      });
-    });
-    it('Should return a successfully created employee', () => {
-      expect(data.body.data.message).toBe('User account successfully created');
-    });
-    it('Should return a status of 201', () => {
-      expect(data.status).toBe(201);
-    });
   });
   describe('POST /auth/signin', () => {
     const data = {};
@@ -211,37 +128,6 @@ describe('API endpoint tests', () => {
       expect(data.body.data.title).toEqual('the changed article title');
     });
   });
-  describe('POST /gifs', () => {
-    const data = {};
-    beforeAll((done) => {
-      const req = Request.post({
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
-        },
-        url: 'http://localhost:3000/api/v1/gifs',
-      }, (err, res, body) => {
-        if (err) throw err;
-        data.status = res.statusCode;
-        data.body = JSON.parse(body);
-        console.log('post gif data', data);
-        done();
-      });
-      const form = req.form();
-      form.append('gifPost', fs.createReadStream('Certificate.jpg'));
-      form.append('gif_title', 'my another gif test');
-      form.append('appr_status', 'true');
-    });
-    it('Should return the gif title upon successful creation', () => {
-      expect(data.body.data.title).toBe('my another gif test');
-    });
-    it('Should return a successful creation message', () => {
-      expect(data.body.status).toBe('success');
-    });
-    it('Should return a status code 201', () => {
-      expect(data.status).toBe(201);
-    });
-  });
   describe('PATCH /articles/:articleId', () => {
     const data = {};
     beforeAll((done) => {
@@ -338,30 +224,6 @@ describe('API endpoint tests', () => {
       expect(data.body.status).toBe('success');
     });
   });
-  describe('DELETE /gifs/:gifId', () => {
-    const data = {};
-    beforeAll((done) => {
-      Request.delete({
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
-        },
-        url: 'http://localhost:3000/api/v1/gifs/22',
-      }, (err, resp, body) => {
-        if (err) throw err;
-        data.status = resp.statusCode;
-        data.body = JSON.parse(body);
-        console.log('the delete body', data);
-        done();
-      });
-    });
-    it('Should delete a gif post', () => {
-      expect(data.body.data.message).toBe('gif post successfully deleted');
-    });
-    it('Should send a status of 200', () => {
-      expect(data.status).toBe(200);
-    });
-  });
   describe('POST /articles/:articleId/comment', () => {
     const data = {};
     beforeAll((done) => {
@@ -386,6 +248,144 @@ describe('API endpoint tests', () => {
     });
     it('Should return the new article comment created', () => {
       expect(data.body.data.comment).toEqual('This is a new article comment');
+    });
+  });
+  describe('POST /gifs', () => {
+    const data = {};
+    beforeAll((done) => {
+      const req = Request.post({
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
+        },
+        url: 'http://localhost:3000/api/v1/gifs',
+      }, (err, res, body) => {
+        if (err) throw err;
+        data.status = res.statusCode;
+        data.body = JSON.parse(body);
+        console.log('post gif data', data);
+        done();
+      });
+      const form = req.form();
+      form.append('gifPost', fs.createReadStream('Certificate.jpg'));
+      form.append('gif_title', 'my another gif test');
+      form.append('appr_status', 'true');
+    });
+    it('Should return the gif title upon successful creation', () => {
+      expect(data.body.data.title).toBe('my another gif test');
+    });
+    it('Should return a successful creation message', () => {
+      expect(data.body.status).toBe('success');
+    });
+    it('Should return a status code 201', () => {
+      expect(data.status).toBe(201);
+    });
+  });
+  describe('POST /auth/create-user', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.post({
+        headers: {
+          'content-type': 'x-www-form-urlencoded',
+          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzM1ODE5MjAsImV4cCI6MTU3NTAyMTkyMH0.js2EySKM5sykjUELWrA3jD2acVSwU_1lDooVIGKbp24',
+        },
+        url: 'http://localhost:3000/api/v1/auth/create-user',
+        form: {
+          firstname: 'bolanle1',
+          lastname: 'adeka1',
+          email: 'bolanle@gmail.com1',
+          employee_password: 'bolanle1',
+          gender: 'female',
+          jobrole: 'I.T',
+          employee_no: 29303941,
+          department: 'Admin',
+        },
+      }, (err, res, body) => {
+        if (err) throw err;
+        data.status = res.statusCode;
+        data.body = JSON.parse(body);
+        console.log('the body', data);
+        done();
+      });
+    });
+    it('Should return a successfully created employee', () => {
+      expect(data.body.data.message).toBe('User account successfully created');
+    });
+    it('Should return a status of 201', () => {
+      expect(data.status).toBe(201);
+    });
+  });
+  describe('DELETE /gifs/:gifId', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.delete({
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
+        },
+        url: 'http://localhost:3000/api/v1/gifs/22',
+      }, (err, resp, body) => {
+        if (err) throw err;
+        data.status = resp.statusCode;
+        data.body = JSON.parse(body);
+        console.log('the delete body', data);
+        done();
+      });
+    });
+    it('Should delete a gif post', () => {
+      expect(data.body.data.message).toBe('gif post successfully deleted');
+    });
+    it('Should send a status of 200', () => {
+      expect(data.status).toBe(200);
+    });
+  });
+});
+
+describe('JWT route protection', () => {
+  let server;
+  beforeAll(() => {
+    server = servers;
+  });
+  describe('unauthenticated user should not access protected routes', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.post({
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'http://localhost:3000/api/v1/articles',
+        form: { title: 'test article', article: 'this is a test article', appr_status: true },
+      }, (err, res, body) => {
+        if (err) console.error(err);
+        data.status = res.statusCode;
+        data.body = body;
+        done();
+      });
+    });
+    it('should return error 401', () => {
+      expect(data.status).toBe(401);
+    });
+  });
+  describe('authenticated user should access protected routes', () => {
+    const data = {};
+    beforeAll((done) => {
+      Request.post({
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
+        },
+        url: 'http://localhost:3000/api/v1/articles',
+        form: { title: 'test articles', article: 'this is a test article', appr_status: true },
+      }, (err, res, body) => {
+        if (err) throw err;
+        data.status = res.statusCode;
+        data.body = JSON.parse(body);
+        done();
+      });
+    });
+    it('Should return status 201', () => {
+      expect(data.status).toBe(201);
+    });
+    it('Should return a user', () => {
+      expect(data.body.data.title).toBe('test articles');
     });
   });
 });
