f24e7816c64a9429cb18fdcdedc73c1b9ba9fe38
Tos4christ <glmmission@hotmail.com>
2019-11-13 14:12:21 +0100

    updated test spec files for test whose values need to change

--- app/spec/spec.js
+++ app/spec/spec.js
@@ -290,13 +290,13 @@ describe('API endpoint tests', () => {
         },
         url: 'http://localhost:3000/api/v1/auth/create-user',
         form: {
-          firstname: 'test',
-          lastname: 'testing',
-          email: 'test@example.com',
-          employee_password: 'tested',
+          firstname: 'test12',
+          lastname: 'testing12',
+          email: 'test12@example.com',
+          employee_password: 'tested12',
           gender: 'female',
           jobrole: 'I.T',
-          employee_no: 112,
+          employee_no: 11212,
           department: 'Admin',
         },
       }, (err, res, body) => {
@@ -321,7 +321,7 @@ describe('API endpoint tests', () => {
           'content-type': 'application/x-www-form-urlencoded',
           authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw',
         },
-        url: 'http://localhost:3000/api/v1/gifs/13',
+        url: 'http://localhost:3000/api/v1/gifs/14',
       }, (err, resp, body) => {
         if (err) throw err;
         data.status = resp.statusCode;
