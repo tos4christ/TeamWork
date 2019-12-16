222e3cf8d9e60798910958909fb017c968287d62
Tos4christ <glmmission@hotmail.com>
2019-12-16 08:55:08 +0100

    chores: check for errors in return of all gifs

--- app/Controllers/gifController.js
+++ app/Controllers/gifController.js
@@ -80,8 +80,10 @@ gifController.getAGif = (req, res, next) => {
 gifController.getAllGif = (req, res, next) => {
   pool.query(gifSchema.getAGif, [req.params.gifId])
     .then((gif) => {
+      console.log('the response from gif', gif)
       pool.query(gifSchema.getAGifComment, [req.params.gifId])
         .then((comments) => {
+          console.log('the response from comment', comments)
           res.status(200).json({
             status: 'success',
             data: {
