407603d49164ab94b37234b922cb39c941db55b6
Tos4christ <glmmission@hotmail.com>
2019-12-25 06:47:41 +0100

    add logic to feed route to return articles and gifs with their respective comments array if it exists

--- app/Controllers/feedController.js
+++ app/Controllers/feedController.js
@@ -1,12 +1,30 @@
 import pool from '../Models/poolConnection';
 import feedSchema from '../Models/feedSchema';
 
-const getFeed = (req, res, next) => {
+const getFeed = async (req, res, next) => {
+  const { rows: articleComment } = await pool.query(feedSchema.articleComments);
+  const {rows: gifComment} = await pool.query(feedSchema.gifComments);
   pool.query(feedSchema.article)
-    .then((article) => {
+    .then((article) => {  
+      const Articles = [];
+      for(let a of article.rows) {
+        const comments = articleComment.filter( ac => ac.articleid == a.id);
+        if(comments.length > 0) {
+          a.comments = comments;
+        }
+        Articles.push(a);
+      }
       pool.query(feedSchema.gif)
         .then((gif) => {
-          const feedRow = [...article.rows, ...gif.rows];
+          const Gifs = [];
+          for(let g of gif.rows) {
+            const comments = gifComment.filter( gc => gc.gifid == g.id);
+            if(comments.length > 0) {
+              g.comments = comments;
+            }
+            Gifs.push(g);
+          }
+          const feedRow = [...Articles, ...Gifs];
           feedRow.sort((a, b) => b.createdOn - a.createdOn);
           res.status(200).json({
             status: 'success',
--- app/Models/poolConnection.js
+++ app/Models/poolConnection.js
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 import { Pool } from 'pg';
+import feedSchema from './feedSchema';
 
 config();
 
@@ -9,5 +10,14 @@ const pool = new Pool({
 });
 
 // pool.query('UPDATE gif_table SET gif_url=$1 WHERE gif_id=9', ['https://res.cloudinary.com/tos4christ/image/upload/v1574065216/f1akv59t1lbjhveeq1aa.gif']);
+// +async function() {
 
+//   const { rows: articleComments } = await pool.query(feedSchema.articleComments);
+    
+//   const { rows: gifComments } = await pool.query(feedSchema.gifComments);
+//   console.log(articleComments, gifComments);
+
+// }()
+
+    
 export default pool;
