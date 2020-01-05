a1422266ed08b4c46793a8740e75460d23517466
Tos4christ <glmmission@hotmail.com>
2020-01-05 19:36:16 +0100

    add user name to response from sign in controller so as to display the user and their activities

--- app/Models/articleSchema.js
+++ app/Models/articleSchema.js
@@ -8,7 +8,7 @@ articleSchema.getTagArticleText = 'select a.article_id as id, a.creation_date as
 
 articleSchema.getAllArticleText = 'select a.employee_id as authorId, a.article_id as id, a.creation_date as createdOn, a.article_title as title, a.article_text as article, ct.appr_status as status, ct.comment_id as commentid, ct.comment_text as comment, ct.creation_date as createdon, ct.employee_id as authorid FROM articles a, comments_table ct, article_comment ac WHERE a.employee_id=$1 and a.article_id=ac.article_id and ac.comment_id=ct.comment_id';
 
-articleSchema.getAllArticleNoComment = 'select a.article_id as id, a.creation_date as createdOn, a.article_title as title, a.article_text as article FROM articles a WHERE a.employee_id=$1 AND a.article_id NOT IN (SELECT article_id FROM article_comment)';
+articleSchema.getAllArticleNoComment = 'select a.employee_id as authorId, a.article_id as id, a.creation_date as createdOn, a.article_title as title, a.article_text as article FROM articles a WHERE a.employee_id=$1 AND a.article_id NOT IN (SELECT article_id FROM article_comment)';
 
 articleSchema.getAnArticleComment = 'SELECT c.comment_id as commentId, c.comment_text as comment, c.employee_id as authorId, c.creation_date as createdOn FROM comments_table c, article_comment ac WHERE ac.article_id=$1 and c.comment_id=ac.comment_id';
 
